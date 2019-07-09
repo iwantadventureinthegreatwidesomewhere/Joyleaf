ebbab968eedefb14e68cdef012b895c632bd9922
Marco <marcoguida724@gmail.com>
2019-07-09 16:13:02 -0400

    iOS justified age ver. and disclaimer text

---
+++ Joyleaf/Joyleaf/Joyleaf.iOS/CustomRenderers/iOSCustomLabel.cs
@@ -0,0 +1,23 @@
+﻿using Joyleaf.CustomControls;
+using Joyleaf.iOS.CustomRenderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomLabel), typeof(iOSCustomLabel))]
+
+namespace Joyleaf.iOS.CustomRenderers
+{
+    public class iOSCustomLabel : LabelRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Label> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.TextAlignment = UITextAlignment.Justified;
+            }
+        }
+    }
+}
---
+++ Joyleaf/Joyleaf/Joyleaf/CustomControls/CustomLabel.cs
@@ -0,0 +1,8 @@
+﻿using Xamarin.Forms;
+
+namespace Joyleaf.CustomControls
+{
+    public class CustomLabel : Label
+    {
+    }
+}
