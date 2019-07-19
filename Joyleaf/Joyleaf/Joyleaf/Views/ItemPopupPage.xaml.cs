94af3747330144978380f41ad242e88b2c64f8c2
Marco <marcoguida724@gmail.com>
2019-07-19 16:02:38 -0400

    flavors + effects

--- Joyleaf/Joyleaf/Joyleaf/Views/ItemPopupPage.xaml.cs
+++ Joyleaf/Joyleaf/Joyleaf/Views/ItemPopupPage.xaml.cs
@@ -16,8 +16,20 @@ namespace Joyleaf.Views
 
             InitializeComponent();
 
-            ItemName.Text = item.Name;
-            ItemDesc.Text = item.Desc;
+            Stack.Children.Add(new Label
+            {
+                FontAttributes = FontAttributes.Bold,
+                FontSize = 25,
+                Margin = new Thickness(0, 0, 0, 3),
+                Text = item.Name,
+                TextColor = Color.FromHex("#333333")
+            });
+
+            StackLayout SpeciesStack = new StackLayout
+            {
+                Margin = new Thickness(0, 0, 0, 3),
+                Orientation = StackOrientation.Horizontal
+            };
 
             if (item.Race == Race.Sativa)
             {
@@ -32,7 +44,7 @@ namespace Joyleaf.Views
                 SpeciesStack.Children.Add(new Label
                 {
                     FontAttributes = FontAttributes.Bold,
-                    FontSize = 16,
+                    FontSize = 15,
                     Text = "SATIVA",
                     TextColor = Color.FromHex("#ffa742"),
                     VerticalOptions = LayoutOptions.Center
@@ -51,7 +63,7 @@ namespace Joyleaf.Views
                 SpeciesStack.Children.Add(new Label
                 {
                     FontAttributes = FontAttributes.Bold,
-                    FontSize = 16,
+                    FontSize = 15,
                     Text = "INDICA",
                     TextColor = Color.FromHex("#774dff"),
                     VerticalOptions = LayoutOptions.Center
@@ -70,12 +82,20 @@ namespace Joyleaf.Views
                 SpeciesStack.Children.Add(new Label
                 {
                     FontAttributes = FontAttributes.Bold,
-                    FontSize = 16,
+                    FontSize = 15,
                     Text = "HYBRID",
                     TextColor = Color.FromHex("#00b368"),
                     VerticalOptions = LayoutOptions.Center
                 });
             }
+
+            Stack.Children.Add(SpeciesStack);
+
+            StackLayout RatingStack = new StackLayout
+            {
+                Margin = new Thickness(0, 0, 0, 10),
+                Orientation = StackOrientation.Horizontal
+            };
 
             string number = "" + ratingScore;
             var characteristic = number.Substring(0, number.IndexOf("."));
@@ -119,6 +139,183 @@ namespace Joyleaf.Views
                 TextColor = Color.Gray,
                 VerticalOptions = LayoutOptions.Center
             });
+
+            Stack.Children.Add(RatingStack);
+
+            if (!String.IsNullOrEmpty(item.Desc))
+            {
+                Stack.Children.Add(new Label
+                {
+                    FontSize = 15,
+                    Margin = new Thickness(0, 0, 0, 10),
+                    Text = item.Desc,
+                    TextColor = Color.FromHex("#333333")
+                });
+            }
+
+            if(item.Flavors != null)
+            {
+                Stack.Children.Add(new Label
+                {
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 17,
+                    Text = "FLAVORS",
+                    TextColor = Color.FromHex("#e349c2")
+                });
+
+                FlexLayout FlavorLayout = new FlexLayout
+                {
+                    AlignContent = FlexAlignContent.Start,
+                    AlignItems = FlexAlignItems.Start,
+                    Direction = FlexDirection.Row,
+                    Margin = new Thickness(0, 0, 0, 10),
+                    Wrap = FlexWrap.Wrap
+                };
+
+                foreach (KeyValuePair<string, string> entry in item.Flavors)
+                {
+                    FlavorLayout.Children.Add(new Frame
+                    {
+                        BackgroundColor = Color.Transparent,
+                        BorderColor = Color.FromHex("#e349c2"),
+                        Content = new Label
+                        {
+                            FontSize = 15,
+                            Margin = new Thickness(15, 5),
+                            Text = entry.Value,
+                            TextColor = Color.FromHex("#e349c2")
+                        },
+                        CornerRadius = 10,
+                        Margin = new Thickness(0, 5, 0, 5),
+                        Padding = 0,
+                        HasShadow = false
+                    });
+
+                    FlavorLayout.Children.Add(new BoxView
+                    {
+                        BackgroundColor = Color.Transparent,
+                        HeightRequest = 0,
+                        WidthRequest = 10
+                    });
+                }
+
+                Stack.Children.Add(FlavorLayout);
+            }
+
+            if(item.Effects.Medical != null || item.Effects.Negative != null || item.Effects.Positive != null)
+            {
+                Stack.Children.Add(new Label
+                {
+                    FontAttributes = FontAttributes.Bold,
+                    FontSize = 17,
+                    Text = "EFFECTS",
+                    TextColor = Color.FromHex("#3269e6")
+                });
+
+                FlexLayout EffectLayout = new FlexLayout
+                {
+                    AlignContent = FlexAlignContent.Start,
+                    AlignItems = FlexAlignItems.Start,
+                    Direction = FlexDirection.Row,
+                    Margin = new Thickness(0, 0, 0, 10),
+                    Wrap = FlexWrap.Wrap
+                };
+
+                if (item.Effects.Positive != null)
+                {
+                    foreach (KeyValuePair<string, string> entry in item.Effects.Positive)
+                    {
+                        EffectLayout.Children.Add(new Frame
+                        {
+                            BackgroundColor = Color.Transparent,
+                            BorderColor = Color.FromHex("#00b368"),
+                            Content = new Label
+                            {
+                                FontSize = 15,
+                                Margin = new Thickness(15, 5),
+                                Text = entry.Value,
+                                TextColor = Color.FromHex("#00b368")
+                            },
+                            CornerRadius = 10,
+                            Margin = new Thickness(0, 5, 0, 5),
+                            Padding = 0,
+                            HasShadow = false
+                        });
+
+                        EffectLayout.Children.Add(new BoxView
+                        {
+                            BackgroundColor = Color.Transparent,
+                            HeightRequest = 0,
+                            WidthRequest = 10
+                        });
+                    }
+                }
+
+                if (item.Effects.Negative != null)
+                {
+                    foreach (KeyValuePair<string, string> entry in item.Effects.Negative)
+                    {
+                        EffectLayout.Children.Add(new Frame
+                        {
+                            BackgroundColor = Color.Transparent,
+                            BorderColor = Color.FromHex("#EC5B55"),
+                            Content = new Label
+                            {
+                                FontSize = 15,
+                                Margin = new Thickness(15, 5),
+                                Text = entry.Value,
+                                TextColor = Color.FromHex("#EC5B55")
+                            },
+                            CornerRadius = 10,
+                            Margin = new Thickness(0, 5, 0, 5),
+                            Padding = 0,
+                            HasShadow = false
+                        });
+
+                        EffectLayout.Children.Add(new BoxView
+                        {
+                            BackgroundColor = Color.Transparent,
+                            HeightRequest = 0,
+                            WidthRequest = 10
+                        });
+                    }
+                }
+
+                if (item.Effects.Medical != null)
+                {
+                    foreach (KeyValuePair<string, string> entry in item.Effects.Medical)
+                    {
+                        EffectLayout.Children.Add(new Frame
+                        {
+                            BackgroundColor = Color.Transparent,
+                            BorderColor = Color.FromHex("#3269e6"),
+                            Content = new Label
+                            {
+                                FontSize = 15,
+                                Margin = new Thickness(15, 5),
+                                Text = entry.Value + " Relief",
+                                TextColor = Color.FromHex("#3269e6")
+                            },
+                            CornerRadius = 10,
+                            Margin = new Thickness(0, 5, 0, 5),
+                            Padding = 0,
+                            HasShadow = false
+                        });
+
+                        EffectLayout.Children.Add(new BoxView
+                        {
+                            BackgroundColor = Color.Transparent,
+                            HeightRequest = 0,
+                            WidthRequest = 10
+                        });
+                    }
+                }
+
+                Stack.Children.Add(EffectLayout);
+            }
+
+
+
         }
     }
 }
