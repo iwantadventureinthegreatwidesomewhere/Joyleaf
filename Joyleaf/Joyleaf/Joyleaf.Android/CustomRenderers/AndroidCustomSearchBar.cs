55fd47b36584dc82ce561f7a9ef52ae69ebe5297
Marco <marcoguida724@gmail.com>
2019-08-30 14:43:00 -0400

    fixed design of android searchbar

---
+++ Joyleaf/Joyleaf/Joyleaf.Android/CustomRenderers/AndroidCustomSearchBar.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using Joyleaf.CustomControls;
+using Joyleaf.Droid.CustomRenderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomSearchBar), typeof(AndroidCustomSearchBar))]
+
+namespace Joyleaf.Droid.CustomRenderers
+{
+    public class AndroidCustomSearchBar : SearchBarRenderer
+    {
+        public AndroidCustomSearchBar(Context context) : base(context)
+        {
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<SearchBar> e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.SetBackgroundResource(Resource.Drawable.SearchBar);
+
+                var plateId = Resources.GetIdentifier("android:id/search_plate", null, null);
+                var plate = Control.FindViewById(plateId);
+                plate.SetBackgroundColor(Android.Graphics.Color.Transparent);
+            }
+        }
+    }
+}
--- Joyleaf/Joyleaf/Joyleaf.Android/Resources/Resource.designer.cs
+++ Joyleaf/Joyleaf/Joyleaf.Android/Resources/Resource.designer.cs
@@ -4677,26 +4677,26 @@ namespace Joyleaf.Droid
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
-			// aapt resource value: 0x7f02014d
-			public const int avd_hide_password_1 = 2130837837;
-			
 			// aapt resource value: 0x7f02014e
-			public const int avd_hide_password_2 = 2130837838;
+			public const int avd_hide_password_1 = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int avd_hide_password_3 = 2130837839;
+			public const int avd_hide_password_2 = 2130837839;
+			
+			// aapt resource value: 0x7f020150
+			public const int avd_hide_password_3 = 2130837840;
 			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
 			
-			// aapt resource value: 0x7f020150
-			public const int avd_show_password_1 = 2130837840;
-			
 			// aapt resource value: 0x7f020151
-			public const int avd_show_password_2 = 2130837841;
+			public const int avd_show_password_1 = 2130837841;
 			
 			// aapt resource value: 0x7f020152
-			public const int avd_show_password_3 = 2130837842;
+			public const int avd_show_password_2 = 2130837842;
+			
+			// aapt resource value: 0x7f020153
+			public const int avd_show_password_3 = 2130837843;
 			
 			// aapt resource value: 0x7f020057
 			public const int BackButtonBlack = 2130837591;
@@ -5391,11 +5391,11 @@ namespace Joyleaf.Droid
 			// aapt resource value: 0x7f02013d
 			public const int notification_icon_background = 2130837821;
 			
-			// aapt resource value: 0x7f02014b
-			public const int notification_template_icon_bg = 2130837835;
-			
 			// aapt resource value: 0x7f02014c
-			public const int notification_template_icon_low_bg = 2130837836;
+			public const int notification_template_icon_bg = 2130837836;
+			
+			// aapt resource value: 0x7f02014d
+			public const int notification_template_icon_low_bg = 2130837837;
 			
 			// aapt resource value: 0x7f02013e
 			public const int notification_tile_bg = 2130837822;
@@ -5425,16 +5425,19 @@ namespace Joyleaf.Droid
 			public const int Search = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int Terms = 2130837831;
+			public const int SearchBar = 2130837831;
 			
 			// aapt resource value: 0x7f020148
-			public const int tooltip_frame_dark = 2130837832;
+			public const int Terms = 2130837832;
 			
 			// aapt resource value: 0x7f020149
-			public const int tooltip_frame_light = 2130837833;
+			public const int tooltip_frame_dark = 2130837833;
 			
 			// aapt resource value: 0x7f02014a
-			public const int Up = 2130837834;
+			public const int tooltip_frame_light = 2130837834;
+			
+			// aapt resource value: 0x7f02014b
+			public const int Up = 2130837835;
 			
 			static Drawable()
 			{
